adfa6e5ba76b40200865ab2d4281d9bf81370bb2
mschpr <m.scheper@stud.phwt.de>
2026-04-09 21:05:13 +0200

    Add qr scanner

--- website/script.js
+++ website/script.js
@@ -1,3 +1,11 @@
-function start() {
-    console.log("Hello, world!")
+import { Html5Qrcode, Html5QrcodeScanner } from "html5-qrcode";
+
+function QRScanner() {
+    const scanner = new Html5QrcodeScanner("reader", { fps: 20 });
+    scanner.render((result) => {
+        console.log(result);
+        scanner.clear();
+    });
 }
+
+window.QRScanner = QRScanner;
